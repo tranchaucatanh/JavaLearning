b5f48c44fb180c51800c5e9ba0b946e1ce529878
CAT <CAT>
2020-04-15 21:53:55 -0700

    Stack and Queue

--- mySweetProgram/src/mySweetProgram/MySweetProgram.java
+++ mySweetProgram/src/mySweetProgram/MySweetProgram.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.Consumer;
@@ -11,48 +12,35 @@ import java.util.function.Consumer;
 
 public class MySweetProgram {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		// Reading file
-		Scanner in = new Scanner(new File("pet.txt"));
-		
-		// create list
-		List<String> petsList = new ArrayList<String>();
-		
-		//Read input to list
-		while (in.hasNext()) {
-			petsList.add(in.nextLine());
-		} 
-		
-		// Simple loop
-		for (int i=0; i<petsList.size(); i++) {
-			System.out.println(petsList.get(i));
-		}
-		
-		
-		// Adding to ArrayList petsList
-		petsList.add("Chicken");
-		petsList.add("Snake");
-		
-
-		//  Range loop
-		for (String pet: petsList) {
-			System.out.println(pet);
-		}
-
-		
-		// Using iterator // check listIterator
-		Iterator<String> petEach = petsList.iterator();
-		
-		while(petEach.hasNext())
-		{
-			System.out.println(petEach.next());
-		}
-		
-		// Using iterator for each 
-		petsList.forEach((pet)->{System.out.println(pet);}); 
-		
-		
-		in.close();
+	public static void main(String[] args){
+			
+	// create list
+	LinkedList<String> namesList = new LinkedList<String>();
+	
+	// Insert at the end BOTTOM of the list QUEUE
+	namesList.add("YOU");
+	namesList.add("LOVE");
+	namesList.add("ME");
+	
+	// Print YOU LOVE ME PRINT BOTTOM FIRST 
+	// can use .pop() = removeFirst(); 
+	// .remove() -> pass index to remove
+	while(!namesList.isEmpty()) {
+		System.out.print(namesList.remove() + " ");
 	}
-
+	
+	// Print [] queue empty
+	System.out.println(namesList);
+	
+	// Insert at the front  of the list STACK 
+	namesList.push("Love");
+	namesList.push("You");
+	namesList.push("Much");
+	
+	for (String name : namesList) {
+	System.out.print(name + " ");
+	}
+	
+	// print Much You Love - LAST IN FIRST OUT FIRST 
+} 
 }
